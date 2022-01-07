22d1e875c588d6c66883aa1306874aa57a4c5bcb
Sesen8 <yonass@spu.edu>
2022-01-07 11:16:54 -0800

    Deleted lines 12-32
    According to instructions I followed them

--- main.cpp
+++ main.cpp
@@ -9,27 +9,7 @@ int main() {
     const int SIZE = 10;
     int position, value;
 
-    value = 20;
-    position = LinearSearch(myData, SIZE, value);
-    if (position == -1)
-        cout << value << " not found in the array" << endl;
-    else
-        cout << value << " found in position " << position << endl;
-
-    value = -5;
-    position = LinearSearch(myData, SIZE, value);
-    if (position == -1)
-        cout << value << " not found in the array" << endl;
-    else
-        cout << value << " found in position " << position << endl;
-
-    value = 4;
-    position = LinearSearch(myData, SIZE, value);
-    if (position == -1)
-        cout << value << " not found in the array" << endl;
-    else
-        cout << value << " found in position " << position << endl;
-
+ 
 
     return 0;
 }
